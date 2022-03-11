a240f54dc0ecda1b36f273141bec6a4e52f7e11e
David <davidkloving@gmail.com>
2022-03-11 10:36:42 -0600

    cleaned up some remaining mess from starter code

--- scoring.py
+++ scoring.py
@@ -1,6 +1,5 @@
 import json
 import logging
-import os
 import pickle
 from pathlib import Path
 
@@ -8,14 +7,19 @@ import pandas as pd
 from sklearn import metrics
 
 
-def score_model(test_data_folder, trained_model_folder):
-    dataset_file_path = Path(test_data_folder) / Path("testdata.csv")
-    logging.info("Reading data from %s", dataset_file_path)
-    test_data = pd.read_csv(dataset_file_path)
+def score_model():
+    with open('config.json', 'r') as f:
+        config = json.load(f)
 
-    model_file_path = Path(trained_model_folder) / Path("trainedmodel.pkl")
-    logging.info("Reading model from %s", trained_model_folder)
-    with open(model_file_path, 'rb') as file:
+    test_data_path = Path(config['test_data_path']) / Path("testdata.csv")
+    trained_model_path = Path(config['output_model_path']) / Path("trainedmodel.pkl")
+    test_result_path = Path(config['output_model_path']) / Path("latestscore.txt")
+
+    logging.info("Reading data from %s", test_data_path)
+    test_data = pd.read_csv(test_data_path)
+
+    logging.info("Reading model from %s", trained_model_path)
+    with open(trained_model_path, 'rb') as file:
         clf = pickle.load(file)
 
     y = test_data["exited"]
@@ -29,20 +33,11 @@ def score_model(test_data_folder, trained_model_folder):
     score = metrics.f1_score(y, y_pred)
     logging.info("Test score: %f", score)
 
-    output_filename = Path(trained_model_folder) / Path("latestscore.txt")
-    logging.info("Writing score to %s", output_filename)
-    with open(output_filename, 'w') as file:
+    logging.info("Writing score to %s", test_result_path)
+    with open(test_result_path, 'w') as file:
         file.write(str(score))
 
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.DEBUG)
-
-    # Load config.json and get path variables
-    with open('config.json', 'r') as f:
-        config = json.load(f)
-
-    trained_model_path = os.path.join(config['output_model_path'])
-    test_data_path = os.path.join(config['test_data_path'])
-
-    score_model(test_data_path, trained_model_path)
+    score_model()
--- training.py
+++ training.py
@@ -1,6 +1,5 @@
 import json
 import logging
-import os
 import pickle
 from pathlib import Path
 
@@ -8,10 +7,15 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
 
-def train_model(input_dataset_path, output_model_path):
-    dataset_file_path = Path(input_dataset_path) / Path("finaldata.csv")
-    logging.info("Reading data from %s", dataset_file_path)
-    dataset = pd.read_csv(dataset_file_path)
+def train_model():
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    dataset_csv_path = Path(config['output_folder_path']) / Path("finaldata.csv")
+    model_path = Path(config['output_model_path']) / Path("trainedmodel.pkl")
+
+    logging.info("Reading data from %s", dataset_csv_path)
+    dataset = pd.read_csv(dataset_csv_path)
     y = dataset["exited"]
     X = dataset[[
         "lastmonth_activity",
@@ -19,33 +23,22 @@ def train_model(input_dataset_path, output_model_path):
         "number_of_employees",
     ]]
 
-    # use this logistic regression for training
     clf = LogisticRegression(
         C=1.0, class_weight=None, dual=False, fit_intercept=True,
-        intercept_scaling=1, l1_ratio=None, max_iter=1000, n_jobs=None, penalty='l2',
+        intercept_scaling=1, l1_ratio=None, max_iter=100, n_jobs=None, penalty='l2',
         random_state=0, solver='liblinear', tol=0.0001, verbose=0,
         warm_start=False
     )
 
-    # fit the logistic regression to your data
     logging.info("Fitting model.")
     clf.fit(X, y)
     logging.info("Score on training data: %f", clf.score(X, y))
 
-    # write the trained model to your workspace in a file called trainedmodel.pkl
-    model_file_path = Path(output_model_path) / Path("trainedmodel.pkl")
-    logging.info("Saving model to %s", model_file_path)
-    with open(model_file_path, 'wb') as file:
+    logging.info("Saving model to %s", model_path)
+    with open(model_path, 'wb') as file:
         pickle.dump(clf, file)
 
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.DEBUG)
-
-    # Load config.json and get path variables
-    with open('config.json', 'r') as f:
-        config = json.load(f)
-
-    dataset_csv_path = os.path.join(config['output_folder_path'])
-    model_path = os.path.join(config['output_model_path'])
-    train_model(dataset_csv_path, model_path)
+    train_model()
