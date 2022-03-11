41bd5b327416a7ebb7ca1587fa617ee94e36305a
David <davidkloving@gmail.com>
2022-03-11 10:50:25 -0600

    added "deployment" functionality

--- deployment.py
+++ deployment.py
@@ -1,27 +1,34 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
-import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
+import logging
+import shutil
+from pathlib import Path
 
 
-
-##################Load config.json and correct path variable
-with open('config.json','r') as f:
-    config = json.load(f) 
-
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
+def copy_to_production(input_folder, output_folder, filename):
+    input_path = Path(input_folder) / Path(filename)
+    output_path = Path(output_folder) / Path(filename)
+    logging.info("Copying %s to %s", input_path, output_path)
+    shutil.copy(input_path, output_path)
 
 
-####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
+def deploy():
+    with open('config.json', 'r') as f:
+        config = json.load(f)
 
+    deployment_folder = Path(config["prod_deployment_path"])
+
+    model_folder = Path(config['output_model_path'])
+    model_name = Path("trainedmodel.pkl")
+    copy_to_production(input_folder=model_folder, output_folder=deployment_folder, filename=model_name)
+
+    test_result_filename = Path("latestscore.txt")
+    copy_to_production(input_folder=model_folder, output_folder=deployment_folder, filename=test_result_filename)
+
+    data_folder = Path(config['output_folder_path'])
+    datset_log_file = Path("ingestedfiles.txt")
+    copy_to_production(input_folder=data_folder, output_folder=deployment_folder, filename=datset_log_file)
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.DEBUG)
+    deploy()
