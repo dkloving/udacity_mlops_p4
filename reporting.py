106c40ede9be0f0693b8a795bc23c8edf1b7ad70
David <davidkloving@gmail.com>
2022-03-22 18:55:58 -0500

    updated confusion matrix plotting to use sqlite

--- reporting.py
+++ reporting.py
@@ -3,34 +3,40 @@ import logging
 from pathlib import Path
 
 import matplotlib.pyplot as plt
-import pandas as pd
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 
+from dbsetup import ProjectDB
 from diagnostics import model_predictions
 
 
-def score_model(test_file_path, model_file_path, output_path):
-    test_df = pd.read_csv(test_file_path)
-    test_predictions = model_predictions(test_df, model_file_path)
-    test_true = test_df["exited"]
+def make_confusion_matrix(test_data, model, save_location=None):
+    """Plots a Confusion Matrix and optionally saves it to disk
+    """
+    test_predictions = model_predictions(test_data, model)
+    test_true = test_data["exited"]
 
     cm = confusion_matrix(test_true, test_predictions)
     cm_plot = ConfusionMatrixDisplay(cm)
     cm_plot.plot()
-    figure_path = output_path / Path("confusionmatrix.png")
-    logging.info("Writing confusion matrix to %s", figure_path)
-    plt.savefig(figure_path)
+
+    if save_location:
+        figure_path = save_location / Path("confusionmatrix.png")
+        logging.info("Writing confusion matrix to %s", figure_path)
+        plt.savefig(figure_path)
 
 
 def run():
     with open('config.json', 'r') as f:
         config = json.load(f)
-
-    test_csv_path = Path(config['test_data_path']) / Path("testdata.csv")
-    saved_model_path = Path(config['prod_deployment_path']) / Path("trainedmodel.pkl")
     output_folder = Path(config["output_model_path"])
 
-    score_model(test_csv_path, saved_model_path, output_folder)
+    db = ProjectDB()
+    dataset_obj = db.get_latest_dataset()
+    dataset = dataset_obj['data']
+    model_obj = db.get_latest_model()
+    model = model_obj['model']
+
+    make_confusion_matrix(dataset, model, output_folder)
 
 
 if __name__ == '__main__':
