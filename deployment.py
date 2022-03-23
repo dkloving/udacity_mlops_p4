58f5a047459705c10c5d807fb51f5d6e8ebaf915
David <davidkloving@gmail.com>
2022-03-22 23:01:00 -0500

    updated deployment to also record metrics required by 'reporting'

--- deployment.py
+++ deployment.py
@@ -26,18 +26,26 @@ def deploy_latest():
     model_obj = db.get_latest_model()
     dataset_obj = db.get_latest_dataset()
     diagnostics = db.get_diagnostics(dataset_obj['id'])
+    summary = db.get_summary(dataset_obj['id'])
 
     model_output_name = deployment_folder / Path("trainedmodel.pkl")
     with open(model_output_name, 'wb') as file:
         pickle.dump(model_obj['model'], file)
 
+    packages_output_name = deployment_folder / Path("packages.csv")
+    with open(packages_output_name, 'w') as file:
+        file.write(diagnostics['packages'])
+
     test_result_name = deployment_folder / Path("latestscore.txt")
     with open(test_result_name, 'w') as file:
-        file.write(str(diagnostics['f1_score']))
+        json.dump({k: diagnostics[k] for k in diagnostics if k != "packages"}, file)
 
     dataset_log_name = deployment_folder / Path("ingestedfiles.txt")
     with open(dataset_log_name, 'w') as file:
         file.write(','.join(dataset_obj['file_list']))
+
+    dataset_summary_name = deployment_folder / Path("dataset_summary.csv")
+    summary.to_csv(dataset_summary_name)
 
 
 if __name__ == "__main__":
