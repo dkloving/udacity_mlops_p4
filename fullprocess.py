23a33e5c2622dd198ba5bc3f7be6f270be9778da
David <davidkloving@gmail.com>
2022-03-23 00:14:37 -0500

    updated fullprocess.py to work with database

--- fullprocess.py
+++ fullprocess.py
@@ -1,5 +1,7 @@
 import json
 import logging
+import logging.config
+import pickle
 from pathlib import Path
 
 import deployment
@@ -8,42 +10,49 @@ import ingestion
 import reporting
 import scoring
 import training
+from dbsetup import ProjectDB
 
 
-def check_model_exists(config):
-    trained_model_path = Path(config['output_model_path']) / Path("trainedmodel.pkl")
-    return trained_model_path.exists()
+def check_model_exists(db: ProjectDB):
+    model_obj = db.get_latest_model()
+    exists = model_obj['model'] is not None
+    return exists
 
 
-def check_new_data(config):
-    ingested_files_path = Path(config["output_folder_path"]) / Path("ingestedfiles.txt")
-    if not ingested_files_path.exists():
+def check_new_data(config, db: ProjectDB):
+    ingested_files = db.get_latest_dataset()['file_list']
+    if ingested_files is None:
         return True
 
     else:
-        with open(ingested_files_path) as f:
-            ingested_files = f.readlines()
-        logging.info("Found %i files were previously ingested.", len(ingested_files) - 1)
+        logging.info("Found %i files were previously ingested.", len(ingested_files))
 
         source_files = list(Path(config["input_folder_path"]).glob("*.csv"))
         logging.info("Found %s source files.", len(source_files))
 
         new_files_present = any([file not in ingested_files for file in source_files])
         logging.info("New files present: %s", 'TRUE' if new_files_present else 'FALSE')
+
         return new_files_present
 
 
-def check_model_drift(config):
+def check_model_drift(config, db: ProjectDB):
     model_score_path = Path(config["prod_deployment_path"]) / Path("latestscore.txt")
     if not model_score_path.exists():
         return True
 
     else:
         with open(model_score_path) as f:
-            recent_score = float(f.read())
+            recent_score = json.load(f)['f1_score']
 
-        new_data_path = Path(config['output_folder_path']) / Path("finaldata.csv")
-        new_score = scoring.score_model(test_data_path=new_data_path)
+        production_model_path = Path(config['prod_deployment_path']) / Path("trainedmodel.pkl")
+        with open(production_model_path, 'rb') as file:
+            model = pickle.load(file)
+
+        new_data = db.get_latest_dataset()['data']
+
+        new_score = scoring.score_model(new_data, model)
+
         drift = new_score < recent_score
         logging.info(
             "New model score (%f) vs previous (%f), drift detected: %s",
@@ -56,36 +65,36 @@ def check_model_drift(config):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s %(levelname)s | %(module)s:%(funcName)s | %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-    )
+    logging.config.fileConfig('logging.conf')
+
+    # initialize database
+    db = ProjectDB()
 
     with open('config.json', 'r') as f:
         config = json.load(f)
 
-    model_exists = check_model_exists(config)
+    model_exists = check_model_exists(db)
     if model_exists:
 
-        new_data = check_new_data(config)
+        new_data = check_new_data(config, db)
         if not new_data:
-            quit()
+            # quit()
+            pass
 
         logging.info("Ingesting new data...")
         ingestion.merge_multiple_dataframe()
 
-        model_drift = check_model_drift(config)
+        model_drift = check_model_drift(config, db)
         if not model_drift:
-            quit()
+            # quit()
+            pass
 
     logging.info("Training and Scoring new model...")
     training.train_model()
-    scoring.write_score()
-
-    logging.info("Deploying...")
-    deployment.deploy()
 
     logging.info("Diagnostics and Reporting...")
     diagnostics.run()
     reporting.run()
+
+    logging.info("Deploying...")
+    deployment.deploy_latest()
